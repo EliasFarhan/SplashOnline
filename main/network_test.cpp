d17f303863bee19f82cf891193f6b0e0a274d821
Elias Farhan <elias.farhan@gmail.com>
2024-08-20 10:33:59 +0200

    Update linux neko net lib and putting bg on network_test

--- main/network_test.cpp
+++ main/network_test.cpp
@@ -1,15 +1,17 @@
 
 #include "engine/engine.h"
 #include "network/client.h"
+#include "graphics/texture_manager.h"
 
 #include <SDL_main.h>
 
-class NetworkTestSystem : public splash::SystemInterface
+class NetworkTestSystem : public splash::SystemInterface, public splash::DrawInterface
 {
 public:
 	explicit NetworkTestSystem(splash::NetworkClient* client): client_(client)
 	{
 		splash::AddSystem(this);
+		splash::AddDrawInterface(this);
 	}
 	void Begin() override
 	{
@@ -17,10 +19,15 @@ public:
 	}
 	void End() override
 	{
+		splash::RemoveDrawInterface(this);
 		splash::RemoveSystem(this);
 	}
 	void Update(float dt) override
 	{
+		if(splash::IsTextureLoaded() && bg_ == nullptr)
+		{
+			bg_ = splash::GetTexture(splash::TextureManager::TextureId::BG);
+		}
 		playerInput_ = splash::GetPlayerInput();
 		if(client_->GetState() == splash::NetworkClient::State::IN_GAME)
 		{
@@ -40,11 +47,36 @@ public:
 	{
 		systemIndex_ = index;
 	}
+
+	void Draw() override
+	{
+		if(bg_)
+		{
+			auto* renderer = splash::GetRenderer();
+			SDL_RenderCopy(renderer,
+				bg_,
+				nullptr,
+				nullptr);
+		}
+	}
+
+	void SetGraphicsIndex(int index) override
+	{
+		graphicsIndex_ = index;
+	}
+
+	int GetGraphicsIndex() const override
+	{
+		return graphicsIndex_;
+	}
+
 private:
 	splash::NetworkClient* client_ = nullptr;
+	SDL_Texture* bg_ = nullptr;
 	int systemIndex_ = -1;
-	splash::PlayerInput playerInput_{};
+	int graphicsIndex_ = -1;
 	int frame_ = 0;
+	splash::PlayerInput playerInput_{};
 };
 
 int main([[maybe_unused]]int argc, [[maybe_unused]]char** argv)
