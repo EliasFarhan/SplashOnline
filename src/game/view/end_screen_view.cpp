6f5c0605ef805d5eab56a209c86387a32c812191
Elias Farhan <elias.farhan@gmail.com>
2025-02-06 22:23:23 +0100

    Use sixit cast when it makes sense

--- src/game/view/end_screen_view.cpp
+++ src/game/view/end_screen_view.cpp
@@ -20,7 +20,7 @@ void EndScreenView::SetPlayerScore(const std::array<int, MaxPlayerNmb>& playerSc
 	{
 		if(IsValid(i))
 		{
-			playerRanks_[i] = static_cast<int>(std::distance(sortedPlayerScores.begin(), std::find(sortedPlayerScores.begin(), sortedPlayerScores.end(), playerScores[i])))+1;
+			playerRanks_[i] = sixit::guidelines::narrow_cast<int>(std::distance(sortedPlayerScores.begin(), std::find(sortedPlayerScores.begin(), sortedPlayerScores.end(), playerScores[i])))+1;
 			const auto textureId = playerRanks_[i] == 1 ?
 								   static_cast<TextureManager::TextureId>(static_cast<int>(TextureManager::TextureId::WIN_P1_CAT)+i) :
 								   static_cast<TextureManager::TextureId>(static_cast<int>(TextureManager::TextureId::LOSE_P1_CAT)+i);
@@ -51,8 +51,8 @@ void EndScreenView::Draw()
 							   static_cast<TextureManager::TextureId>(static_cast<int>(TextureManager::TextureId::LOSE_P1_CAT)+i);
 		const auto textureSize = static_cast<neko::Vec2<float>>(GetTextureSize(textureId))/pixelPerMeter;
 		const neko::Vec2f size = neko::Vec2f(neko::Vec2<float>(gameWindowSize))/neko::Scalar {pixelPerMeter};
-		neko::Scalar x = neko::Scalar{static_cast<float>(i+1)}*size.x/neko::Scalar(sixit::guidelines::precision_cast<float>(playerCount_+1))-size.x/neko::Scalar{2};
-		neko::Scalar y = neko::Scalar{static_cast<float>((playerCount_+1)-playerRanks_[i])}*size.y/neko::Scalar(sixit::guidelines::precision_cast<float>(playerCount_+1))-size.y/neko::Scalar{2};
+		neko::Scalar x = neko::Scalar{sixit::guidelines::precision_cast<float>(i+1)}*size.x/neko::Scalar(sixit::guidelines::precision_cast<float>(playerCount_+1))-size.x/neko::Scalar{2};
+		neko::Scalar y = neko::Scalar{ sixit::guidelines::precision_cast<float>(playerCount_+1-playerRanks_[i])}*size.y/neko::Scalar(sixit::guidelines::precision_cast<float>(playerCount_+1))-size.y/neko::Scalar{2};
 		y = neko::Lerp(neko::Scalar{}, y, static_cast<neko::Scalar>(transitionTimer_.CurrentRatio()));
 
 		const auto rect = GetDrawingRect({x, y}, neko::Vec2f(textureSize));
